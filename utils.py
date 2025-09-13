8355f67289cd53c04973a5fa71c27f1a3dbe409c
Jaimin Raval <jaiminraval100@gmail.com>
2025-09-13 16:12:39 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 # utils.py
 # A new file with a simple utility function to demonstrate the CI/CD test runner.
 
-def subtract(self, a, b):
-        """
-        Subtracts the second number from the first.
+def sum_two_numbers(a, b):
+    """
+    Calculates the sum of two numbers.
 
-        Args:
-            a (int or float): The first number.
-            b (int or float): The second number.
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
 
-        Returns:
-            int or float: The result of a - b.
-        """
-        return a - b
+    Returns:
+        int or float: The sum of the two numbers.
+    """
+    return a + b
