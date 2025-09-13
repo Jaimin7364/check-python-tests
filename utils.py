115a50de76a70929157d16cc67789406f631ffdf
Jaimin Raval <jaiminraval100@gmail.com>
2025-09-13 13:46:35 +0530

    commit groq

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 # utils.py
 # A new file with a simple utility function to demonstrate the CI/CD test runner.
 
-def sum_two_numbers(a, b):
+def mul_two_numbers(a, b):
     """
-    Calculates the sum of two numbers.
+    Calculates the mul of two numbers.
 
     Args:
         a (int or float): The first number.
         b (int or float): The second number.
 
     Returns:
-        int or float: The sum of the two numbers.
+        int or float: The mul of the two numbers.
     """
-    return a + b
+    return a * b
